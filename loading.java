ab58966753e85ac3de74988dae9ce92583283a81
KangJinWoo0228 <rkdwlsdn979696@gmail.com>
2022-04-14 10:44:42 +0900

    안드로이드 깃 테스트

---
+++ loading.java
@@ -0,0 +1,24 @@
+package com.example.tracer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(loading.this, main.class);
+                startActivity(intent);
+            }
+        }, 1000);
+    }
+}
---
+++ main.java
@@ -0,0 +1,31 @@
+package com.example.tracer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+
+public class main extends AppCompatActivity {
+
+    Button btn_login;
+    EditText edt_id, edt_pw;
+
+    RequestQueue requestQueue;
+    StringRequest request;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        btn_login = findViewById(R.id.btn_login);
+        edt_id = findViewById(R.id.edt_id);
+        edt_pw = findViewById(R.id.edt_pw);
+
+
+    }
+}
